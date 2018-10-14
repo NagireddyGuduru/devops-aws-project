905573047bd76a03b152046063626086aa32fa89
nagireddybitbucket <nagireddy.devops2018@gmail.com>
2018-10-14 12:24:54 +0530

    deploy to tomcat

--- myweb-project/src/main/java/in/javahome/myweb/controller/Calculator.java
+++ myweb-project/src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,27 +1,25 @@
 package in.javahome.myweb.controller;
-
 /*
  * 
  */
 public class Calculator {
-	   public int add(int number1, int number2) 
-   {
-     if (number1> number2)
-	   return number1 + number2;
-	   else 
-		   return 10101010;
-   }
-   
-   public int sub(int number1, int number2) {
-      return number1 - number2;
-   }
-   
-   public int mul(int number1, int number2) {
-      return number1 * number2;
-   }
-   
-   public int div(int number1, int number2) {
-      return number1 / number2;
-   }
-   
+	
+	
+	
+	
+	
+	
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		String s = null;
+		s.toString();
+		return i+j+10;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
 }
